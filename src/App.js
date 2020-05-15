1c610bb7fac012b2c64f191f0bc0dbedbe84c2f1
ayomideonabajo <ayo4real2009@gmail.com>
2020-05-16 00:07:30 +0100

    added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import  Person from './Person/Person';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      
+      <div><h2 > it's the new react on the new system</h2>
+       <grte/>
+      </div>
+     
+   
+   </div>
+    
+   
+  );
+ 
+}
+
+export default App;
